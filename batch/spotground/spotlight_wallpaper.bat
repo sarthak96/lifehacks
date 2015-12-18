5bb3651f8ae85e86da8c38a032cad20b00ebf449
sarthak96 <sarthakm27@gmail.com>
2015-12-18 22:39:12 +0530

    Initial Commit

---
+++ batch/spotground/spotlight_wallpaper.bat
@@ -0,0 +1,14 @@
+@echo off
+rem Get native screen resolution - Not tried on multimonitor
+FOR /f "delims=" %%q IN ('%comspec% /c "wmic desktopmonitor get ScreenWidth,ScreenHeight" /value ^| find "="') DO (SET %%q)
+SET res=%ScreenWidth%%ScreenHeight%
+SET "dest=%USERPROFILE%\Pictures\Spotlight\"
+SET "source=%USERPROFILE%\AppData\Local\Packages\Microsoft.Windows.ContentDeliveryManager_cw5n1h2txyewy\LocalState\Assets"
+FOR /R %source% %%F IN (*.*) DO (
+	FOR /f %%a in ('Wia.bat %%F')  DO (
+		IF "%%a" == "%res%" (
+			echo F|XCOPY  "%%F" "%dest%%%~nF.jpeg" /d /y /q > nul
+		)
+	) 
+)
+EXIT /B /0
